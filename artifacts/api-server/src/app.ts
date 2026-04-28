a9a5d5543afbb7843827abed3f3a0b47c353b696
player343 <73027628+player343@users.noreply.github.com>
2026-04-28 21:09:36 +0100

    Refactor imports and logger initialization in app.ts

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,15 +1,10 @@
-import express, { type Express } from "express";
+import express, { type Express, type Request, type Response } from "express";
 import cors from "cors";
-import pinoHttp = require("pino-http");
-
-import type { Request, Response } from "express";
-
+import pinoHttp from "pino-http";
 import router from "./routes";
 import { logger as customLogger } from "./lib/logger";
 
 const app: Express = express();
-
-const logger = pinoHttp();
 
 app.use(
   pinoHttp({
@@ -34,7 +29,6 @@ app.use(
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use("/api", router);
 
 export default app;
